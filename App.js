2824e216ee7ee2ce6cd7d4f058cff89a943b0682
gandoo <m_altyeib@yahoo.com>
2020-04-21 02:54:32 +0200

    fix some bug in data API

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import DeckList from "./components/DeckList";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <DeckList />
     </View>
   );
 }
